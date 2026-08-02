2fab215d728946f1112da93a26ef06c1cc6737ad
omhade100-afk <omhade100@gmail.com>
2026-08-02 23:26:48 +0530

    Delete /js directory

--- css/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const btn = document.getElementById('themeToggle');
-    btn.addEventListener('click', () => {
-        const theme = document.body.getAttribute('data-theme') === 'light' ? 'dark' : 'light';
-        document.body.setAttribute('data-theme', theme);
-        btn.innerText = theme === 'light' ? '🌙' : '☀️';
-    });
-});
