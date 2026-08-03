592de67163912470421246c92c63dc3438a203fb
omhade100-afk <omhade100@gmail.com>
2026-08-03 06:58:10 +0530

    Delete css directory

--- css/js/script.js
+++
@@ -1,8 +0,0 @@
-console.log("System Initialized");
-const btn = document.getElementById('themeToggle');
-if(btn) {
-    btn.addEventListener('click', () => {
-        const theme = document.body.getAttribute('data-theme') === 'light' ? 'dark' : 'light';
-        document.body.setAttribute('data-theme', theme);
-    });
-}
